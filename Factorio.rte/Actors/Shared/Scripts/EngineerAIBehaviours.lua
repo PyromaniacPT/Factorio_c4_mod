8f076af80647f077aa2602819bba69ac80d051ef
Bebomonky <bebomonky@gmail.com>
2022-07-06 01:02:07 -0400

    Ear Orgasm Update

--- Factorio.rte/Actors/Shared/Scripts/EngineerAIBehaviours.lua
+++ Factorio.rte/Actors/Shared/Scripts/EngineerAIBehaviours.lua
@@ -1,8 +1,7 @@
 EngineerAIBehaviours = {};
 
 function EngineerAIBehaviours.handleMovement(self)
-	
-	local crouching = self.controller:IsState(Controller.BODY_CROUCH)
+
 	local moving = self.controller:IsState(Controller.MOVE_LEFT) or self.controller:IsState(Controller.MOVE_RIGHT);
 	
 	--NOTE: you could also put in things like your falling scream here easily with very little overhead
@@ -25,68 +24,110 @@ function EngineerAIBehaviours.handleMovement(self)
 			local mat = nil
 			local pixelPos = footPos + Vector(0, 4)
 			local checkPix = SceneMan:GetTerrMatter(pixelPos.X, pixelPos.Y);
-
-			if checkPix == 128 or checkPix == 130 then
+--			   Grass
+			if checkPix == 128 then
 				if (moving) then
-					if (self.moveSoundWalkTimer:IsPastSimMS(450)) then -- Lower is less delay / Faster is more delay
+					if (self.moveSoundWalkTimer:IsPastSimMS(455)) then -- Lower is less delay / Faster is more delay
 						self.environmentSounds.Grass:Play(self.Pos)
 						self.moveSoundWalkTimer:Reset();
 					end
+				elseif self.wasInAir then
+					if (self.feetContact[1] == true or self.feetContact[2] == true) and self.fakejumpstop:IsPastSimMS(100) then
+						self.wasInAir = false;
+						if self.Vel.Y > 0 and self.moveSoundTimer:IsPastSimMS(500) then
+							self.environmentSounds.landGrass:Play(self.Pos);
+							self.moveSoundTimer:Reset();
+						end
+					end
 				end
-
+--				   Vegetation
+			elseif checkPix == 130 then
+				if (moving) then
+					if (self.moveSoundWalkTimer:IsPastSimMS(455)) then
+						self.environmentSounds.Grass2:Play(self.Pos)
+						self.moveSoundWalkTimer:Reset();
+					end
+				elseif self.wasInAir then
+					if (self.feetContact[1] == true or self.feetContact[2] == true) and self.fakejumpstop:IsPastSimMS(100) then
+						self.wasInAir = false;
+						if self.Vel.Y > 0 and self.moveSoundTimer:IsPastSimMS(500) then
+							self.environmentSounds.landGrass:Play(self.Pos);
+							self.moveSoundTimer:Reset();
+						end
+					end
+				end
+--				   Bedrock			Cave Ceiling	   Concrete
 			elseif checkPix == 13 or checkPix == 15 or checkPix == 177 then
 				if (moving) then
-					if (self.moveSoundWalkTimer:IsPastSimMS(450)) then
+					if (self.moveSoundWalkTimer:IsPastSimMS(455)) then
 						self.environmentSounds.Concrete:Play(self.Pos)
 						self.moveSoundWalkTimer:Reset();
 					end
+				elseif self.wasInAir then
+					if (self.feetContact[1] == true or self.feetContact[2] == true) and self.fakejumpstop:IsPastSimMS(100) then
+						self.wasInAir = false;
+						if self.Vel.Y > 0 and self.moveSoundTimer:IsPastSimMS(500) then
+							self.environmentSounds.landConcrete:Play(self.Pos);
+							self.moveSoundTimer:Reset();
+						end
+					end
 				end
-
+--				   Stone			Dense Red Earth	   Wet Concrete		   Metal			 Mega Metal		  Mangled Metal
 			elseif checkPix == 12 or checkPix == 16 or checkPix == 164 or checkPix == 178 or checkPix == 179 or checkPix == 180 then
 				if (moving) then
-					if (self.moveSoundWalkTimer:IsPastSimMS(450)) then
+					if (self.moveSoundWalkTimer:IsPastSimMS(455)) then
 						self.environmentSounds.Concrete2:Play(self.Pos)
 						self.moveSoundWalkTimer:Reset();
 					end
+				elseif self.wasInAir then
+					if (self.feetContact[1] == true or self.feetContact[2] == true) and self.fakejumpstop:IsPastSimMS(100) then
+						self.wasInAir = false;
+						if self.Vel.Y > 0 and self.moveSoundTimer:IsPastSimMS(500) then
+							self.environmentSounds.landConcrete2:Play(self.Pos);
+							self.moveSoundTimer:Reset();
+						end
+					end
 				end
-
+--				   	Earth			Dense Earth		  Cave Floor
 			elseif checkPix == 10 or checkPix == 11 or checkPix == 14 then
 				if (moving) then
-					if (self.moveSoundWalkTimer:IsPastSimMS(450)) then
+					if (self.moveSoundWalkTimer:IsPastSimMS(455)) then
 						self.environmentSounds.Dirt:Play(self.Pos)
 						self.moveSoundWalkTimer:Reset();
 					end
+				elseif self.wasInAir then
+					if (self.feetContact[1] == true or self.feetContact[2] == true) and self.fakejumpstop:IsPastSimMS(100) then
+						self.wasInAir = false;
+						if self.Vel.Y > 0 and self.moveSoundTimer:IsPastSimMS(500) then
+							self.environmentSounds.landDirt:Play(self.Pos);
+							self.moveSoundTimer:Reset();
+						end
+					end
 				end
-
+--				   	Snow			  Sand			 Topsoil
 			elseif checkPix == 6 or checkPix == 8 or checkPix == 9 then
 				if (moving) then
-					if (self.moveSoundWalkTimer:IsPastSimMS(450)) then
+					if (self.moveSoundWalkTimer:IsPastSimMS(455)) then
 						self.environmentSounds.Sand:Play(self.Pos)
 						self.moveSoundWalkTimer:Reset();
 					end
+				elseif self.wasInAir then
+					if (self.feetContact[1] == true or self.feetContact[2] == true) and self.fakejumpstop:IsPastSimMS(100) then
+						self.wasInAir = false;
+						if self.Vel.Y > 0 and self.moveSoundTimer:IsPastSimMS(500) then
+							self.environmentSounds.landSand:Play(self.Pos);
+							self.moveSoundTimer:Reset();
+						end
+					end
 				end
 			end
-			--if materialID.PresetName == self.Concrete then
-			--	self.environmentSounds.Concrete:Play(self.Pos);
-			--	
-			--	elseif materialID.PresetName == self.Dirt then
-			--	self.environmentSounds.Dirt:Play(self.Pos);
-			--	
-			--	elseif materialID.PresetName == self.Sand then
-			--	self.environmentSounds.Sand:Play(self.Pos);
-	
-			--end		
-			--PrimitiveMan:DrawLinePrimitive(pixelPos, pixelPos, 13)
+
 			if self.footPixel ~= 0 then
 				mat = SceneMan:GetMaterialFromID(checkPix)
-			--	PrimitiveMan:DrawLinePrimitive(pixelPos, pixelPos, 162);
-			--else
-			--	PrimitiveMan:DrawLinePrimitive(pixelPos, pixelPos, 13);
 			end
 			
 			local movement = (self.controller:IsState(Controller.MOVE_LEFT) == true or self.controller:IsState(Controller.MOVE_RIGHT) == true or self.Vel.Magnitude > 3)
 			if mat ~= nil then
-				--PrimitiveMan:DrawTextPrimitive(footPos, mat.PresetName, true, 0);
 				if self.feetContact[i] == false then
 					self.feetContact[i] = true
 					if self.feetTimers[i]:IsPastSimMS(self.footstepTime) and movement then																	
@@ -103,5 +144,12 @@ function EngineerAIBehaviours.handleMovement(self)
 			end
 		end
 	end
+
+	if self.Vel.Y > 10 then
+		self.wasInAir = true;
+	else
+		self.wasInAir = false;
+	end
+
 	self.wasMoving = moving;
 end
--- Factorio.rte/Actors/Shared/Scripts/EnvironmentInter.lua
+++ Factorio.rte/Actors/Shared/Scripts/EnvironmentInter.lua
@@ -8,15 +8,26 @@ function Create(self)
 
 	self.environmentSounds = {
         Concrete = CreateSoundContainer("Engineer Step Concrete", "Factorio.rte");
+        landConcrete = CreateSoundContainer("Engineer Land Concrete", "Factorio.rte");
         Concrete2 = CreateSoundContainer("Engineer Step Concrete 2", "Factorio.rte");
+        landConcrete2 = CreateSoundContainer("Engineer Land Concrete 2", "Factorio.rte");
+
         Dirt = CreateSoundContainer("Engineer Step Dirt", "Factorio.rte");
+        landDirt = CreateSoundContainer("Engineer Land Dirt", "Factorio.rte");
+
         Sand = CreateSoundContainer("Engineer Step Sand", "Factorio.rte");
-        Grass = CreateSoundContainer("Engineer Step Grass", "Factorio.rte")};
+        landSand = CreateSoundContainer("Engineer Land Sand", "Factorio.rte");
+
+        Grass = CreateSoundContainer("Engineer Step Grass", "Factorio.rte");
+        landGrass = CreateSoundContainer("Engineer Land Grass", "Factorio.rte");
+        Grass2 = CreateSoundContainer("Engineer Step Grass 2", "Factorio.rte")};
 
     self.feetContact = {false, false}
     self.feetTimers = {Timer(), Timer()}
+    self.moveSoundTimer = Timer();
     self.moveSoundWalkTimer = Timer();
-    self.wasCrouching = false;
+    self.fakejumpstop = Timer();
+    self.wasInAir = false;
     self.wasMoving = false;
     self.footstepTime = 100 -- 2 Timers to avoid noise
 
