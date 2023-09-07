3ed60fa370c0632e51fd551759ca23cd32a7a9a5
Bebomonky <bebomonky@gmail.com>
2023-09-06 21:20:01 -0400

    I told Pyro bad thing and it brokie the mod
    I am stoopid

--- Factorio.rte/Devices/Tools/Engineer-Tool/Engineer-Tool.lua
+++ Factorio.rte/Devices/Tools/Engineer-Tool/Engineer-Tool.lua
@@ -562,8 +562,8 @@ end
 
 function ScreenPos(self, PosX, PosY)
 	local Screen = Vector(
-		SceneMan:GetOffset(self.Team).X + PosX,
-		SceneMan:GetOffset(self.Team).Y + PosY
+		CameraMan:GetOffset(self.Team).X + PosX,
+		CameraMan:GetOffset(self.Team).Y + PosY
 	)
 	return Screen
 end
