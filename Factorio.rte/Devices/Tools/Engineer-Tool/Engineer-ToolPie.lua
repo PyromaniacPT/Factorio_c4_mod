292bc65911c26bd266cb0fcfb7193ff3bcb7011b
Bebomonky <bebomonky@gmail.com>
2023-09-09 17:44:14 -0400

    Removed Menu - Now will Require Depedency
    Dependency
    https://github.com/Bebomonky/InteractiveMenu

--- Factorio.rte/Devices/Tools/Engineer-Tool/Engineer-ToolPie.lua
+++ Factorio.rte/Devices/Tools/Engineer-Tool/Engineer-ToolPie.lua
@@ -1,6 +1,13 @@
-function FactorioBuyMenu(pieMenuOwner, pieMenu, pieSlice)
+function FactorioEngineerMenu(pieMenuOwner, pieMenu, pieSlice)
 	local gun = pieMenuOwner.EquippedItem
 	if gun then
-		ToMOSRotating(gun):SetNumberValue("ActiveMenu", 1)
+		ToMOSRotating(gun):SetNumberValue("ActiveEngineerMenu", 1)
+	end
+end
+
+function FactorioAssemblyMenu(pieMenuOwner, pieMenu, pieSlice)
+	local gun = pieMenuOwner.EquippedItem
+	if gun then
+		ToMOSRotating(gun):SetNumberValue("ActiveAssemblyMenu", 1)
 	end
 end
