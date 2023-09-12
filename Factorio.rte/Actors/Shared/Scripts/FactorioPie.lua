8997643e3967f5a6341f1ab5d0d64c827dfa503b
Bebomonky <bebomonky@gmail.com>
2023-09-12 15:33:00 -0400

    Pie Menu Improvements
    No need for duplicate Pie files - Removed AIPie.lua
    Remove Duplicate Pie Icon sprites (Bsae.rte already has this?)
    Move PieIcons to Actors/Shared/PieIcons/

--- Factorio.rte/Actors/Shared/Scripts/FactorioPie.lua
+++ Factorio.rte/Actors/Shared/Scripts/FactorioPie.lua
@@ -1,3 +1,5 @@
+-- Engineer Tool
+
 function FactorioEngineerMenu(pieMenuOwner, pieMenu, pieSlice)
 	local gun = pieMenuOwner.EquippedItem
 	if gun then
@@ -5,9 +7,25 @@ function FactorioEngineerMenu(pieMenuOwner, pieMenu, pieSlice)
 	end
 end
 
+-- Machines
+
 function FactorioAssemblyMenu(pieMenuOwner, pieMenu, pieSlice)
 	local gun = pieMenuOwner.EquippedItem
 	if gun then
 		ToMOSRotating(gun):SetNumberValue("ActiveAssemblyMenu", 1)
 	end
+end
+
+-- Drones
+
+function PatrolAIMode(self)
+	self:SetNumberValue("AIMode", 0)
+end
+
+function FollowAIMode(self)
+	self:SetNumberValue("AIMode", 1)
+end
+
+function SentryAIMode(self)
+	self:SetNumberValue("AIMode", 2)
 end
